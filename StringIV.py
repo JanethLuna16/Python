4aac162464b09c73ebf24ec9a5c461590c6cd696
JanethLuna16 <111544516+JanethLuna16@users.noreply.github.com>
2022-08-30 13:56:59 -0500

    Practica 5

---
+++ StringIV.py
@@ -0,0 +1,7 @@
+
+#!/usr/bin/env phyton
+# _*_ coding: cp1252 _*_           #Librerias para los acentos
+# _*_ coding: 850 _*_
+# _*_ coding: utf-8 _*_  
+
+print("\t-Phyton. \n\t-JavaScript. \n\t-Java. \n\t-PHP. \n\t-TypeScirpt. \n\t-SQL. \n\t-COBOL.\n\t-C++.\n\t-C#.")
