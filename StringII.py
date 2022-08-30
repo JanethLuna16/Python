269b39919321a3d7c00baa8075d8c5af7e039ba1
JanethLuna16 <111544516+JanethLuna16@users.noreply.github.com>
2022-08-30 13:54:57 -0500

    Practica 3

---
+++ StringII.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/env phyton
+# _*_ coding: cp1252 _*_           #Librerias para los acentos
+# _*_ coding: 850 _*_
+# _*_ coding: utf-8 _*_  
+
+concatenar ="Hola" +" "+"Mundo"    #Concatenar dos strings en una sola variable
+print(concatenar)
+
+var1="Hello"                       #Concatenar dos strings dos variables e imprimir el resultado
+var2=" World"
+conca = var1 + var2 
+print (conca)
+
+Nombre="Janeth"                     #Concatenar dos strings en dos variables , concatenar los espacios e imprimirlos
+Apellido="Luna"
+Nombrecom = Nombre +" "+Apellido
+print (Nombrecom)
+
+print("33 " + " 12" )               #Concatenar dos int pero no sumarlos
