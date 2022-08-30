f774818f592df6bc4fc7351e961102eba4168652
JanethLuna16 <111544516+JanethLuna16@users.noreply.github.com>
2022-08-30 13:54:17 -0500

    Practica 2

---
+++ Strings.py
@@ -0,0 +1,5 @@
+
+Frase1="'print()' se utiliza para imprimir valores en la consola."  #Se utilizan comillas simples para resaltar una palabra dentro del string
+Frase2='"print()" se utiliza para imprimir valores en la consola.'  #Se utilizan comillas dobles para resaltar una palabra dentro del string
+print(Frase1) #Impresion de la frase1
+print(Frase2) #Impresion de la frase1
