36b78725f1d72c0ba76c4190431f2fe55370d432
JanethLuna16 <111544516+JanethLuna16@users.noreply.github.com>
2022-08-30 13:53:12 -0500

    Practica 1

---
+++ Variables.py
@@ -0,0 +1,7 @@
+mensaje = "Hello world"         #Asignamos a la variable un string
+mensaje2 = 20                   #Asignamos a la variable un int
+mensaje3 = 500                  #Asignamos a la variable un int
+suma = mensaje2 + mensaje3      #Se asigna a suma el valor del mensaje2 mas el valor de mensaje3
+print(mensaje)                  #Impresion de variable mensaje
+print(mensaje2+100)             #Impresion de variable mensaje2 mas 100
+print(suma)                     #Impresion de variable suma
