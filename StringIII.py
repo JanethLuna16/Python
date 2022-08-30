2573eb87343fb92bf0eea1630372e0a6d941a44d
JanethLuna16 <111544516+JanethLuna16@users.noreply.github.com>
2022-08-30 13:56:29 -0500

    Practica 4

---
+++ StringIII.py
@@ -0,0 +1,13 @@
+
+#!/usr/bin/env phyton
+# _*_ coding: cp1252 _*_           #Librerias para los acentos
+# _*_ coding: 850 _*_
+# _*_ coding: utf-8 _*_  
+
+titulo="janeth luna"                                #Uso del title para colocar la primera letra de cada palabra en mayúsculas
+print(titulo.title())
+
+frase="Esta Es Una Frase Para Ser Formateada."      #Uso del lower para poner todas las letras a minúsculas
+print(frase.lower())
+
+print(frase.upper())                                #Uso del Upper para poner todas las letras de la frase en Mayusculas
