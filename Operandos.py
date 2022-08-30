1cd96a9808fd8fd7756e6641a43d4e28843ba869
JanethLuna16 <111544516+JanethLuna16@users.noreply.github.com>
2022-08-30 13:57:42 -0500

    Practica 6

---
+++ Operandos.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env phyton
+# _*_ coding: cp1252 _*_           #Librerias para los acentos
+# _*_ coding: 850 _*_
+# _*_ coding: utf-8 _*_  
+
+Suma= 16+28
+resta=16-28
+multi=16*28
+divi=16/28
+exp=4**2
+print( " Suma \t\t",Suma, "\n" ,"Resta\t\t",resta,"\n","Multiplicación\t",multi,"\n","División\t",divi,"\n","Exponencial\t",exp )
