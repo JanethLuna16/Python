2c3f28317b8be7efb0a135b516ddc433eae917df
JanethLuna16 <111544516+JanethLuna16@users.noreply.github.com>
2022-09-05 11:56:20 -0500

    Practica 19

--- Variables_y_Funciones_Anidadas.py
+++ Variables_y_Funciones_Anidadas.py
@@ -1,17 +1,17 @@
 
-def local():
+def local():                                                    #Funcion para demostración de las variables local, solo se pueden utilizar dentro de la funcion 
     variable1 = "Variable local"
-    global variable2 
+    global variable2                                            #Conversion de una variable local a una variable global
     variable2 ="Conversion de variable local a global"
     print(variable1)
 
-    def anidada():
+    def anidada():                                              #Variable local anidada dentro de otra función
         variable1 = "Variable local anidada en una funcion"
         print(variable1)
    
-    anidada()
+    anidada()                                                   #Dentro de la función se manda a llamar la función anidada
 
-local()
-variable_global = "Variable global"
+local()                                                         #Llamar a la fuunción principal para poder imprirmir las variables que esta contiene
+variable_global = "Variable global"                             #Creación de una variable global, se puede utilizar el todo el programa
 print(variable_global)
 print(variable2)
