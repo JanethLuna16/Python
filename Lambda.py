26d54b5121de41f30b1673a15296e90eed480114
JanethLuna16 <111544516+JanethLuna16@users.noreply.github.com>
2022-09-05 11:59:41 -0500

    Practica 20

--- Lambda.py
+++ Lambda.py
@@ -1,13 +1,13 @@
 import math
 
 
-def area(radio):
+def area(radio):                                                        #Codigo para calcular el area de un circulo sin Lambda, (Sin Lambda se utilizan mas lineas de codigo
     print("Codigo sin Lambda")
     resultado = round(math.pi * radio * radio,3)
     print(resultado)
     
     
 area(2)
-print("Codigo Con Lambda")
+print("Codigo Con Lambda")                                                #Codigo para calcular el area de un circulo con Lambda, (Con Lambda se utilizan menos  lineas de codigo
 area= lambda radio: round(math.pi * radio * radio,3)
 print(area(2))
