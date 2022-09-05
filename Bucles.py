99888898a46fe219d833b8cffcd999b75e13a170
JanethLuna16 <111544516+JanethLuna16@users.noreply.github.com>
2022-09-05 11:38:24 -0500

    Practica 13

--- Bucles.py
+++ Bucles.py
@@ -1,13 +1,13 @@
 
-a=5
+a=5                                     #Se inicializa la variable A =5
 
-while a<15:
+while a<15:                             #El buble se realiza hasta que a sea menor a 15
     a+=1
-    if(a==12 or a==13 or a==14):
+    if(a==12 or a==13 or a==14):        #Si a llega el valor de 12,13 o 14 , no los imprime y continua con el while
         continue
-    print(a)
+    print(a)                            #Impresion de variable fuera del if
 
 
-print("Bucle terminado")
+print("Bucle terminado")                
 
 
